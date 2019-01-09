89dcadad55971bea909084e8f1da94940b68124c
sydnash <sydnash1@gmail.com>
2019-01-09 14:05:05 +0800

    [fix]

--- Assets/Client/Lua/ui/doushisiDesk/doushisiOperation.lua
+++ Assets/Client/Lua/ui/doushisiDesk/doushisiOperation.lua
@@ -2137,10 +2137,10 @@ end
 function doushisiOperation:computeFlyTime(x1, y1, x2, y2) 
     local d1 = math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)
     local dis = math.sqrt(d1)
-    local speed = 13.00 --pixels per second
+    local speed = 12.50 --pixels per second
     local time = dis / speed
-    if time < 0.25 then
-        time = 0.25
+    if time < 0.23 then
+        time = 0.23
     end
     if time > 0.35 then
         time = 0.35
