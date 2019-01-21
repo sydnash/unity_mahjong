d6a3d5193281175eb39b8803f7243aaadd76cc1e
xieheng <xieheng84@163.com>
2019-01-21 19:31:08 +0800

    修改公众号名字

--- Assets/Client/Lua/ui/shop.lua
+++ Assets/Client/Lua/ui/shop.lua
@@ -7,7 +7,7 @@ local shop = class("shop", base)
 
 _RES_(shop, "ShopUI", "ShopUI")
 
-local COPY_TO_CLIPBOARD_STRING = "天地川牌斗十四"
+local COPY_TO_CLIPBOARD_STRING = "天地川牌"
 
 function shop:onInit()
     self.mClose:addClickListener(self.onCloseClickedHandler, self)
