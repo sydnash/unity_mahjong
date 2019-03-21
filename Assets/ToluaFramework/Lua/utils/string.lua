c945fd282b37bc9047d7e7c24a71608ff23d7323
sydnash <sydnash1@gmail.com>
2019-03-21 09:44:34 +0800

    [fix] 增加urlencode decode

--- Assets/ToluaFramework/Lua/utils/string.lua
+++ Assets/ToluaFramework/Lua/utils/string.lua
@@ -67,4 +67,15 @@ function string.trim(input)
     return string.gsub(input, "[ \t\n\r]+$", "")
 end
 
+function strting.urlEncode(s)
+    s = string.gsub(s, "([^%w%.%- ])", function(c) return string.format("%%%02X", string.byte(c)) end)
+    return string.gsub(s, " ", "+")
+end
+ 
+local function urlDecode(s)
+    s = string.gsub(s,"+"," ")
+    s = string.gsub(s, '%%(%x%x)', function(h) return string.char(tonumber(h, 16)) end)
+    return s
+end
+
 --endregion
